de71a340447d93cb0bb2facc2b0ef11ff568a2a3
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-04-01 12:40:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function fibonacci(num) {
-	let a=0,b=1;
+	let a=0,b=1,c;
 if (num == 1) {
 	return a;
 }
 if (num==2) {
 	return b;
 }
-else{
-	return fibonacci(num-1)+fibonacci(num-2);
+for (let i = 3; i <= num; i++) {
+	c=a+b;
+	a=b;
+	b=c;
 }
+	return b;
 }
 
 module.exports = fibonacci;
