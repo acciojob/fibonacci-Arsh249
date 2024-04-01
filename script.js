98ef0ea15c811fa4b31cbdf7369fbc42c0d013a3
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-04-01 12:30:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function fibonacci(num) {
-	
-if (num <= 1) {
-	return num;
-}else
+	let a=0,b=1;
+if (num == 1) {
+	return a;
+}
+if (num==2) {
+	return b;
+}
+else{
 	return fibonacci(num-1)+fibonacci(num-2);
+}
 }
 
 module.exports = fibonacci;
